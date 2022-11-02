4218cbe303718443f62ffee807168b1bd2765720
Boonsrp029 <49550816+Boonsrp029@users.noreply.github.com>
2022-11-02 12:19:10 +0700

    Nodemon update

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const morgan = require('morgan')
+/* eslint-disable @typescript-eslint/no-var-requires */
+const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const port = 3000;
 
-const app = express()
-app.use(morgan('combine'))
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
+app.use(morgan("combine"));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get('status', (req, res) => {
-    res.status({
-        message: "hello world"
-    })
-})
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
-app.listen(process.env.PORT || 8081)
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
